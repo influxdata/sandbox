910e6e8b5e0379db4b5f8eba9fef61ac6f81cc46
timhallinflux <timhallinflux@users.noreply.github.com>
2018-03-29 11:10:35 -0700

    Create wait_for_influxdb.sh
    Wait loop to pause startup for ifql startup

---
+++ images/ifqld_wait/wait_for_influxdb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# wait_for_influxdb.sh
+
+set -e
+
+until wget http://influxdb:8086/ping; do
+  echo "InfluxDB is unavailable - sleeping"
+  sleep 5
+done
+
+echo "InfluxDB is up - Starting IFQL"
